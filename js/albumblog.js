95a06a3a2b15586765e1cfd38e984376b00c16d4
Album <a.buffolino+blog@gmail.com>
2020-08-29 11:26:31 +0200

    Try to add comment counter.

--- js/albumblog.js
+++ js/albumblog.js
@@ -5,3 +5,11 @@ function sendMail() {
   aValue = aMail.getAttribute("value");
   aMail.setAttribute("href", "mailto:"+atob(aValue));
 }
+
+function isLocal() {
+  if (["localhost", "127.0.0.1"].indexOf(window.location.hostname) != -1) {
+    return true;
+  } else {
+    return false;
+  }
+}
